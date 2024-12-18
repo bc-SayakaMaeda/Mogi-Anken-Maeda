240c220a6238bb36a5b3f8b2b0ff40947005f1bb
bc-SayakaMaeda <sayaka.maeda@mail.benesse.co.jp>
2024-12-18 11:44:30 +0900

    インデント調整：レビュー1回目修正　完了

--- web-template/web-template/src/main/resources/static/js/webCustomerLogin.js
+++ web-template/web-template/src/main/resources/static/js/webCustomerLogin.js
@@ -1,46 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
-	const form = document.getElementById('webCustomerLoginForm');
-	const userIdInput = document.getElementById('userId');
-	const passwordInput = document.getElementById('password');
-	const errorMessageDiv = document.querySelector('.error-message');
-	const togglePassword = document.getElementById('togglePassword');
-	
-	form.addEventListener('submit', function(event) {
-		event.preventDefault();
-		let isValid = true;
-		errorMessageDiv.innerHTML = '';
-		// 必須チェック
-		if (!userIdInput.value || !passwordInput.value) {
-			errorMessageDiv.innerHTML = '入力必須項目です。';
-			errorMessageDiv.style.visibility = 'visible'; 
-			isValid = false;
-		}
-		
-		// フォーマットチェック
-		const alphanumericRegex = /^[a-zA-Z0-9]*$/;
-		if (!alphanumericRegex.test(userIdInput.value) || !alphanumericRegex.test(passwordInput.value)) {
-			errorMessageDiv.innerHTML = '入力形式が間違っています。';
-			errorMessageDiv.style.visibility = 'visible'; 
-			isValid = false;
-			}
-		// 入力チェックに問題がない場合フォームを送る
-		if (isValid) {
-			form.submit(); 
-		}
-	});
-	
-	// パスワード表示機能
-	togglePassword.addEventListener('click', function() {
-		const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-		passwordInput.setAttribute('type', type);
-		this.src = type === 'password' ? '../static/images/icon_eye_hide.png' : '../static/images/icon_eye.png';
-	});
-	
-	passwordInput.addEventListener('input', function() {
-		if (passwordInput.value) {
-			togglePassword.style.display = 'block'; 
-		} else {
-			togglePassword.style.display = 'none'; 
-		}
-	});
+    const form = document.getElementById('webCustomerLoginForm');
+    const userIdInput = document.getElementById('userId');
+    const passwordInput = document.getElementById('password');
+    const errorMessageDiv = document.querySelector('.error-message');
+    const togglePassword = document.getElementById('togglePassword');
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        let isValid = true;
+        errorMessageDiv.innerHTML = '';
+        // 必須チェック
+        if (!userIdInput.value || !passwordInput.value) {
+            errorMessageDiv.innerHTML = '入力必須項目です。';
+            errorMessageDiv.style.visibility = 'visible'; 
+            isValid = false;
+        }
+        
+        // フォーマットチェック
+        const alphanumericRegex = /^[a-zA-Z0-9]*$/;
+        if (!alphanumericRegex.test(userIdInput.value) || !alphanumericRegex.test(passwordInput.value)) {
+            errorMessageDiv.innerHTML = '入力形式が間違っています。';
+            errorMessageDiv.style.visibility = 'visible'; 
+            isValid = false;
+            }
+        // 入力チェックに問題がない場合フォームを送る
+        if (isValid) {
+            form.submit(); 
+        }
+    });
+    
+    // パスワード表示機能
+    togglePassword.addEventListener('click', function() {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+        this.src = type === 'password' ? '../static/images/icon_eye_hide.png' : '../static/images/icon_eye.png';
+    });
+    
+    passwordInput.addEventListener('input', function() {
+        if (passwordInput.value) {
+            togglePassword.style.display = 'block'; 
+        } else {
+            togglePassword.style.display = 'none'; 
+        }
+    });
 });

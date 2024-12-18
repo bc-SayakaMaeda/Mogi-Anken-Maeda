77d3752f9a6b4bc28c88f000b41fe652dac24cd3
bc-SayakaMaeda <sayaka.maeda@mail.benesse.co.jp>
2024-12-19 07:15:08 +0900

    Javadocの追加、インデントの調整

--- web-template/web-template/src/main/java/jp/co/benesse/web/controller/WebCustomerLoginController.java
+++ web-template/web-template/src/main/java/jp/co/benesse/web/controller/WebCustomerLoginController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import jakarta.servlet.http.HttpSession;
 
 /**
- *  <pre>
+ * <pre>
  * web利用者ログインコントローラークラス
  *
  * 作成日：2024/12/17
- * 更新日：2024/12/17
+ * 更新日：2024/12/18
  * </pre>
  *
  * @author BC)maeda
@@ -21,19 +21,21 @@ import jakarta.servlet.http.HttpSession;
 public class WebCustomerLoginController {
 
     /**
-     * @param session
-     * @param model
+     * 初期表示：web利用者ログイン画面表示 1. セッション情報を初期化 2. 入力フィールド（利用者ID・パスワード）を初期化
+     * 
+     * @param session セッション
+     * @param model モデル
      * @return web利用者ログイン画面
      */
     @GetMapping("/webCustomerLogin")
     public String showWebCustomerLogin(HttpSession session, Model model) {
         // セッション情報を初期化
         session.invalidate();
-        
-        // 入力フィールドを初期化
+
+        // 入力フィールド（利用者ID・パスワード）を初期化
         model.addAttribute("userId", "");
         model.addAttribute("password", "");
-        
-        return "webCustomerLogin"; 
+
+        return "webCustomerLogin";
     }
 }

30d139cc0d8b68e7e49a6df7469ac3f55a732434
bc-SayakaMaeda <sayaka.maeda@mail.benesse.co.jp>
2024-12-19 08:31:36 +0900

    ログインボタン押下時のコントローラー追加、テーブル定義にあわせてuserIdをcustomerIDに変更

--- web-template/web-template/src/main/java/jp/co/benesse/web/controller/WebCustomerLoginController.java
+++ web-template/web-template/src/main/java/jp/co/benesse/web/controller/WebCustomerLoginController.java
@@ -3,6 +3,8 @@ package jp.co.benesse.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -33,9 +35,62 @@ public class WebCustomerLoginController {
         session.invalidate();
 
         // 入力フィールド（利用者ID・パスワード）を初期化
-        model.addAttribute("userId", "");
+        model.addAttribute("customerID", "");
         model.addAttribute("password", "");
 
         return "webCustomerLogin";
+    }
+    
+    /**
+     * ログインボタン押下時：ログイン処理 
+     * 1. ユーザー情報取得 
+     * 2. パスワードのハッシュ化 
+     * 3. DBアクセス（ログイン判定情報取得）
+     * 4. セッション保存 
+     * 5. 画面遷移
+     * 
+     * @param customerID インプットの利用者ID
+     * @param password インプットのパスワード
+     * @param model モデル
+     * @param session セッション
+     * @return メニュー画面
+     */
+    @PostMapping("/webCustomerLogin")
+    public String login(@RequestParam("customerID") String customerID, @RequestParam("password") String password, Model model, HttpSession session) {
+        // ユーザー情報取得
+        // 必須チェック
+        if (customerID == null || customerID.isEmpty() || password == null || password.isEmpty()) {
+            model.addAttribute("errorMessage", "入力必須項目です。");
+            return "webCustomerLogin";
+        }
+
+        // 文字列長チェック
+        if (customerID.length() < 8 || customerID.length() > 16 || password.length() < 8 || password.length() > 16) {
+            model.addAttribute("errorMessage", "IDまたはパスワードの入力が適切ではありません。");
+            return "webCustomerLogin";
+        }
+
+        // フォーマットチェック
+        if (!customerID.matches("[a-zA-Z0-9]+") || !password.matches("[a-zA-Z0-9]+")) {
+            model.addAttribute("errorMessage", "IDまたはパスワードの入力が適切ではありません。");
+            return "webCustomerLogin";
+        }
+
+        // ログイン処理
+        WebCustomer webCustomer = webCustomerLoginService.login(customerID, password);
+        if (webCustomer == null) {
+            model.addAttribute("errorMessage", "IDまたはパスワードが間違っています。");
+            return "webCustomerLogin";
+        }
+
+        // セッション保存
+        session.setAttribute("customerID", webCustomer.getCustomerId());
+        session.setAttribute("customerName", webCustomer.getCustomerName());
+        session.setAttribute("postCode", webCustomer.getPostCode());
+        session.setAttribute("address", webCustomer.getAddress());
+        session.setAttribute("email", webCustomer.getEmail());
+
+        // 画面遷移
+        return "forward:/webCustomerMenu";
     }
 }

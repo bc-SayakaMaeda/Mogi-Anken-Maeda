aafd41de6c1f4fd20dee8402caa78870d1b77947
bc-SayakaMaeda <sayaka.maeda@mail.benesse.co.jp>
2024-12-27 15:08:52 +0900

    レビュー修正2回目四次：半角英数字の正規表現を定数定義

--- web-template/web-template/src/main/resources/static/js/constants.js
+++ web-template/web-template/src/main/resources/static/js/constants.js
@@ -10,3 +10,8 @@ const IMAGE_PATHS = {
     ICON_EYE_HIDE: '../static/images/icon_eye_hide.png',
     ICON_EYE_SHOW: '../static/images/icon_eye.png'
 };
+
+// 正規表現
+const REGEX = {
+    HALF_ALPHANUMERIC: /^[a-zA-Z0-9]*$/
+};

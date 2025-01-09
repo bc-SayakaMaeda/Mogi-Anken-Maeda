ba8dec1ef09a569bb76245edc9601292efdedb74
bc-SayakaMaeda <sayaka.maeda@mail.benesse.co.jp>
2025-01-09 13:46:35 +0900

    Javadocの修正

--- web-template/web-template/src/main/java/jp/co/benesse/web/util/HashUtil.java
+++ web-template/web-template/src/main/java/jp/co/benesse/web/util/HashUtil.java
@@ -3,10 +3,9 @@ package jp.co.benesse.web.util;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-
 /**
  * <pre>
- * Hash化用ユーティリティ.
+ * Hash化に関するユーティリティ
  *
  * 作成日：2025/01/07
  * 更新日：2025/01/07
@@ -18,33 +17,34 @@ import java.security.NoSuchAlgorithmException;
 public class HashUtil {
 
     /**
-    * <pre>
-    * SHA-256でハッシュ化する
-    * </pre>
-    *
-    * @param password
-    * @return ハッシュ化されたパスワード（16進数の文字列）
-    * @throws RuntimeException
-    */
+     * <pre>
+     * SHA-256でハッシュ化する
+     * </pre>
+     *
+     * @param password
+     * @return ハッシュ化されたパスワード（16進数の文字列）
+     * @throws RuntimeException
+     */
     public static String sha256(String password) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(password.getBytes());
             StringBuilder hexString = new StringBuilder();
             for (byte b : hash) {
-                
+
                 // 各バイトを符号なし整数として扱い、StringBuilderで16進数の文字列に変換する
                 String hex = Integer.toHexString(0xff & b);
-                
+
                 // 1桁の場合先頭に0を足して2桁にする
-                if (hex.length() == 1) hexString.append('0');
-              
+                if (hex.length() == 1)
+                    hexString.append('0');
+
                 hexString.append(hex);
             }
-            
+
             // 16進数の文字列を返す
             return hexString.toString();
-            
+
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }

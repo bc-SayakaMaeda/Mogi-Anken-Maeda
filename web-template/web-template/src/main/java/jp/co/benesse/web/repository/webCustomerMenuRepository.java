793fd01d1894687925cd0abc4bd0f4623a8f528d
bc-SayakaMaeda <sayaka.maeda@mail.benesse.co.jp>
2025-01-21 09:36:40 +0900

    図書情報取得のためのメニューリポジトリの作成

--- web-template/web-template/src/main/java/jp/co/benesse/web/repository/webCustomerMenuRepository.java
+++ web-template/web-template/src/main/java/jp/co/benesse/web/repository/webCustomerMenuRepository.java
@@ -1,29 +1,54 @@
 package jp.co.benesse.web.repository;
+
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class webCustomerMenuRepository {
+import jp.co.benesse.web.constants.UrlConstants;
+import jp.co.benesse.web.dto.BookDTO;
 
+/**
+ * <pre>
+ * メニューリポジトリ
+ *
+ * 作成日：2025/01/21
+ * 更新日：2025/01/21
+ * </pre>
+ *
+ * @author BC)maeda
+ * @version 1.0
+ */
+@EnableAutoConfiguration
+@Repository
+public class WebCustomerMenuRepository extends SqlGeneratorBaseRepository {
+
+    /** JDBCテンプレート */
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    /**
+     * 図書情報取得
+     * 
+     * @return DBから取得した書籍情報
+     */
     public List<BookDTO> findAllBooks() {
-        String sql = new String(Files.readAllBytes(Paths.get("src/main/resources/sql/BookRepository/findAllBooks.sql")),
-                StandardCharsets.UTF_8);
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(BookDTO.class));
-    }
+        try {
+            // SQLファイルの内容を読み込む
+            String sql = new String(Files.readAllBytes(Paths.get(UrlConstants.SQL_FIND_ALL_BOOKS)),
+                    StandardCharsets.UTF_8);
 
-    public int countStock(String bookID) {
-        String sql = new String(Files.readAllBytes(Paths.get("src/main/resources/sql/BookRepository/countStock.sql")),
-                StandardCharsets.UTF_8);
-        return jdbcTemplate.queryForObject(sql, new Object[] { bookID }, Integer.class);
+            // SQLクエリを実行し、結果をBookDTOクラスのリストとして返す
+            return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(BookDTO.class));
+
+        } catch (Exception e) {
+            throw new RuntimeException("SQLファイルを読み込めませんでした", e);
+        }
     }
 }

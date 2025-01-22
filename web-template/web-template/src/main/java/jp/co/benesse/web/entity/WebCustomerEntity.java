b2b4a2beb821bc198b88c2670295530b16863f10
bc-SayakaMaeda <sayaka.maeda@mail.benesse.co.jp>
2025-01-22 11:10:37 +0900

    再レビュー修正

--- web-template/web-template/src/main/java/jp/co/benesse/web/entity/WebCustomerEntity.java
+++ web-template/web-template/src/main/java/jp/co/benesse/web/entity/WebCustomerEntity.java
@@ -1,7 +1,5 @@
 package jp.co.benesse.web.entity;
 
-import java.time.LocalDateTime;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +8,7 @@ import lombok.Setter;
  * web利用者ログインエンティティ
  *
  * 作成日：2024/12/24
- * 更新日：2024/12/24
+ * 更新日：2025/01/22
  * </pre>
  * 
  * @author BC)maeda
@@ -32,27 +30,6 @@ public class WebCustomerEntity {
     /** 住所 */
     private String address;
 
-    /** 電話番号 */
-    private String telephone;
-
     /** メールアドレス */
     private String email;
-
-    /** パスワード */
-    private String password;
-
-    /** 論理削除フラグ */
-    private String logicDelFlg;
-
-    /** 作成者 */
-    private String createBy;
-
-    /** 作成日時 */
-    private LocalDateTime createTime;
-
-    /** 更新者 */
-    private String updateBy;
-
-    /** 更新日時 */
-    private LocalDateTime updateTime;
 }

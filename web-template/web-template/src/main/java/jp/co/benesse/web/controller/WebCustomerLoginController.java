1af9269ea7656586f4289ad62ae1863e4b8cb0f0
bc-SayakaMaeda <sayaka.maeda@mail.benesse.co.jp>
2025-01-27 08:25:34 +0900

    2回目レビュー修正：コントローラー内のメソッドの切り分け

--- web-template/web-template/src/main/java/jp/co/benesse/web/controller/WebCustomerLoginController.java
+++ web-template/web-template/src/main/java/jp/co/benesse/web/controller/WebCustomerLoginController.java
@@ -30,7 +30,6 @@ import jp.co.benesse.web.validationGroups.ValidationGroups.RequiredCheck;
  * 更新日：2025/01/20
  * </pre>
  *
- * @author BC)maeda
  * @version 1.0
  */
 @Controller
@@ -85,27 +84,7 @@ public class WebCustomerLoginController {
             BindingResult bindingResult, Model model) {
 
         // バリデーションチェック
-        // 必須チェック
-        validator.validate(webCustomerLoginForm, bindingResult, RequiredCheck.class);
-        if (bindingResult.hasErrors()) {
-            String errorRequired = MessageUtil.getMessage("error.required");
-            model.addAttribute("errorMessage", errorRequired);
-            return UrlConstants.VIEW_WEB_CUSTOMER_LOGIN;
-        }
-
-        // 文字列長チェック
-        validator.validate(webCustomerLoginForm, bindingResult, LengthCheck.class);
-        if (bindingResult.hasErrors()) {
-            String errorLoginLength = MessageUtil.getMessage("error.login.length", "IDまたはパスワード");
-            model.addAttribute("errorMessage", errorLoginLength);
-            return UrlConstants.VIEW_WEB_CUSTOMER_LOGIN;
-        }
-
-        // フォーマットチェック
-        validator.validate(webCustomerLoginForm, bindingResult, FormatCheck.class);
-        if (bindingResult.hasErrors()) {
-            String errorLoginFormat = MessageUtil.getMessage("error.login.format", "IDまたはパスワード");
-            model.addAttribute("errorMessage", errorLoginFormat);
+        if (!validateForm(webCustomerLoginForm, bindingResult, model)) {
             return UrlConstants.VIEW_WEB_CUSTOMER_LOGIN;
         }
 
@@ -136,5 +115,41 @@ public class WebCustomerLoginController {
             return UrlConstants.VIEW_WEB_CUSTOMER_LOGIN;
         }
 
+    }
+
+    /**
+     * フォームのバリデーションチェックを行う
+     * 
+     * @param webCustomerLoginForm web利用者ログインフォーム
+     * @param bindingResult formクラスでのバリデーション結果
+     * @param model モデル
+     * @return バリデーションエラーがない場合はtrue、エラーがある場合はfalse
+     */
+    private boolean validateForm(WebCustomerLoginForm webCustomerLoginForm, BindingResult bindingResult, Model model) {
+        // 必須チェック
+        validator.validate(webCustomerLoginForm, bindingResult, RequiredCheck.class);
+        if (bindingResult.hasErrors()) {
+            String errorRequired = MessageUtil.getMessage("error.required");
+            model.addAttribute("errorMessage", errorRequired);
+            return false;
+        }
+
+        // 文字列長チェック
+        validator.validate(webCustomerLoginForm, bindingResult, LengthCheck.class);
+        if (bindingResult.hasErrors()) {
+            String errorLoginLength = MessageUtil.getMessage("error.login.length", "IDまたはパスワード");
+            model.addAttribute("errorMessage", errorLoginLength);
+            return false;
+        }
+
+        // フォーマットチェック
+        validator.validate(webCustomerLoginForm, bindingResult, FormatCheck.class);
+        if (bindingResult.hasErrors()) {
+            String errorLoginFormat = MessageUtil.getMessage("error.login.format", "IDまたはパスワード");
+            model.addAttribute("errorMessage", errorLoginFormat);
+            return false;
+        }
+
+        return true;
     }
 }

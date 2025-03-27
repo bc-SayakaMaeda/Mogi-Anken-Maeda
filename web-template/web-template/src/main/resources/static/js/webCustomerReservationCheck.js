41712cb1f9ae1c0e67d134263699e79da4ebfe7c
bc-SayakaMaeda <sayaka.maeda@mail.benesse.co.jp>
2025-03-27 13:26:41 +0900

    貸出予約確認画面：初期表示初回作成完了

--- web-template/web-template/src/main/resources/static/js/webCustomerReservationCheck.js
+++ web-template/web-template/src/main/resources/static/js/webCustomerReservationCheck.js
@@ -1,32 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const tableBody = document.getElementById('book-row-block'); // `book-row-block`をターゲットに設定
+document.addEventListener('DOMContentLoaded', function() {
+    const tableBody = document.getElementById('book-row-block');
 
     // テーブルに書籍情報を表示
     function renderTable() {
-        // テーブルの内容をクリア
+
         tableBody.innerHTML = '';
 
-        // 書籍情報をループして表示
         bookList.forEach((item, index) => {
-            const row = document.createElement('tr'); // 新しい行を作成
+            const row = document.createElement('tr');
 
-            // 書籍情報を設定
             row.innerHTML = `
                 <td>${index + 1}</td>
                 <td>${item.title}</td>
                 <td>${item.author}</td>
+                <td>${item.stockCount}</td>
+                <td>
+                    <input type="checkbox" class="large-checkbox" checked>
+                </td>      
             `;
-
-            // 行をテーブルに追加
             tableBody.appendChild(row);
         });
     }
 
     // 初期化処理
     function init() {
-        renderTable(); // 書籍情報を表示
+        renderTable();
     }
 
-    // 初期化を実行
     init();
 });
